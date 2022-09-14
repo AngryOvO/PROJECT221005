dd8bb9f0d8cefb336d230c0d4c6dca624668eee6
AngryOvO <101005580+AngryOvO@users.noreply.github.com>
2022-09-14 13:59:30 +0900

    HW1

---
+++ example2-2/src/CircleArea.java
@@ -0,0 +1,12 @@
+public class CircleArea
+{
+    public static void main(String[] args)
+    {
+        final double PI = 3.14;
+        int radius = 10;
+
+        double circlearea = radius * radius * PI;
+
+        System.out.println("원의 넓이 = " + circlearea);
+    }
+}
